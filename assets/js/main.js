7b80e5449ad6d2116918076bf015af145caf2276
TommyGBazin <113591898+TommyGBazin@users.noreply.github.com>
2023-04-22 16:08:04 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,50 +54,6 @@ project__items.hover(
 
   });
 
-
-          // cursor custom
-
-  var cursor = document.querySelector('.cursor');
-  var cursorinner = document.querySelector('.cursor2');
-  var a = document.querySelectorAll('a');
-  
-  document.addEventListener('mousemove', function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-  });
-  
-  document.addEventListener('mousemove', function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    cursorinner.style.left = x + 'px';
-    cursorinner.style.top = y + 'px';
-  });
-  
-  document.addEventListener('mousedown', function(){
-    cursor.classList.add('click');
-    cursorinner.classList.add('cursorinnerhover')
-  });
-  
-  document.addEventListener('mouseup', function(){
-    cursor.classList.remove('click')
-    cursorinner.classList.remove('cursorinnerhover')
-  });
-  
-  a.forEach(item => {
-    item.addEventListener('mouseover', () => {
-      cursor.classList.add('hover');
-    });
-    item.addEventListener('mouseleave', () => {
-      cursor.classList.remove('hover');
-    });
-  });
-
-  document.body.style.cursor = 'none'
-    document.body.querySelectorAll("button, label, input, textarea, select, a").forEach((el) => {
-      el.style.cursor = 'inherit'
-    });
-
 // Apparition de la page
 
 window.addEventListener('load', function() {
