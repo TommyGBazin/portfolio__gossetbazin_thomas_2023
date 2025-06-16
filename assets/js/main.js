5a04dd56529d1199d107efe021ed94e420a2dad9
TommyGBazin <113591898+TommyGBazin@users.noreply.github.com>
2025-06-16 11:43:53 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,29 @@
 // declaration d une variable
-let project__items =  $('.item__project'),
-close__btn = $('.project .close');
-
-project__items.hover(
-    function() {
-      let obj__project = $(this).data('link-card');
-
-      $('.'+obj__project).css('z-index','999');
-      $('.'+obj__project).addClass('js-animation-hover-project');
-      $(this).addClass('hover-link');
-      $('.item__project').not($( ".item__project.hover-link" )).addClass('hover-active-link');
-
-    }, function() {
-      let obj__project = $(this).data('link-card');
-      $('.project').css('z-index','10');
-      $('.'+obj__project).removeClass('js-animation-hover-project');
-      
-      $(this).removeClass('hover-link');
-      $('.item__project').removeClass('hover-active-link');
-    }
-);
+let project__items = $('.item__project'),
+    close__btn = $('.project .close');
 
 // Click on the project
+project__items.on('click', function(){
+  let obj__project = $(this).data('link-card');
+  $('.project').removeClass('js-animation-click-project');
+  $('.'+obj__project).addClass('js-animation-click-project');
+  $('body').addClass('project-open');
+});
 
-  project__items.on('click', 
-    function(){
-      let obj__project = $(this).data('link-card');
-
-      $('.project').removeClass('js-animation-click-project');
-      $('.'+obj__project).addClass('js-animation-click-project');
-      $('body').addClass('card-open');
-      $('body').addClass('project-open');
-  });
-  
-  close__btn.on('click', 
-    function(){   
-      $(this).closest('.project').removeClass('js-animation-click-project');
-      $('body').removeClass('project-open');
-    // $('.item__project').not(this).removeClass('js-click-none');
-      $('body').removeClass('card-open');
-  });
+close__btn.on('click', function(){   
+  $(this).closest('.project').removeClass('js-animation-click-project');
+  $('body').removeClass('project-open');
+});
 
 // Click outside
-
-  $('body').on('click', function(e){
-
-    if( $('body').hasClass("card-open") )
-    {
-
-      if($(e.target).closest('.container__projects').length === 0  &&  $('body').hasClass("card-open") && e.target.parentNode.className !== 'js-cards-show' )
-      {
-        $('body').find('.project.js-animation-click-project').removeClass('js-animation-click-project');
-        $('body').removeClass('project-open');
-      }
+$('body').on('click', function(e){
+  if($('body').hasClass("project-open")) {
+    if($(e.target).closest('.container__projects').length === 0 && e.target.parentNode.className !== 'js-cards-show') {
+      $('.project.js-animation-click-project').removeClass('js-animation-click-project');
+      $('body').removeClass('project-open');
     }
-
-  });
+  }
+});
 
 // Apparition de la page
 
@@ -82,7 +50,7 @@ setInterval(function() {
 }, 2000);
 
 const words = ["Designer UI UX", "Directeur Artistique", "Développeur Front-End", "Chef de Projet"];
-const colors = ['#AB75E8', '#3FBC9F', '#FF6C51'];
+const colors = ['#fff', '#fff', '#FFf'];
 
 let i = 0;
 const loop = () => {
